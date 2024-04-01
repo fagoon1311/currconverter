db5e856b8d21a6b1d05e35da5dd866d73953cac0
fagoon1311 <fs14smailbox65@gmail.com>
2024-04-01 23:28:16 +0530

    c

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react'
+import Dropdown from './Dropdown'
 
 // https://api.frankfurter.app/currencies
 // /latest?amount=10&amp;from=GBP&amp;to=USD`
 const Converter = () => {
     const [currencies, setCurrencies] = useState([])
     const [amount, setAmount] = useState(1)
+    const [fromCurrency, setFromCurrency] = useState(['USD'])
+    const [toCurrency, setToCurrency] = useState('INR')
 
     useEffect(()=>{
         getCurrency()
@@ -13,7 +16,7 @@ const Converter = () => {
     const getCurrency = async() => {
         const data = await fetch('https://api.frankfurter.app/currencies')
         const json = await data.json()
-        console.log(Object.keys(json))
+        setCurrencies(Object.keys(json))
     }
 
     const currencyConverter = () =>{}
@@ -21,7 +24,26 @@ const Converter = () => {
     <div className='max-w-xl mx-auto my-10 p-5 bg-white rounded-lg shadow-md'>
      
         <h2 className='mb-5 text-gray-700 text-2xl'> Currency Converter</h2>        
-        <div>Dropdowns</div>
+        <div>
+            <Dropdown
+                // favorites={favorites}
+                currencies={currencies}
+                title="From:"
+                //   currency={fromCurrency}    
+                //   setCurrency={setFromCurrency}
+                //   handleFavorite={handleFavorite}
+            />
+            <button>Swap</button>
+            <Dropdown
+                // favorites={favorites}
+                currencies={currencies}
+                title="To:"
+                //   currency={fromCurrency}    
+                //   setCurrency={setFromCurrency}
+                //   handleFavorite={handleFavorite}
+            />
+
+        </div>
         <div>
             <label className='block text-gray-700 font-medium text-sm p-2'>Amount:</label>
             <input onChange={(e)=>setAmount(e.target.value)}
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import { HiOutlineStar } from "react-icons/hi2";
 
-const Dropdown = () => {
+const Dropdown = ({
+    currencies,
+    currency,
+    setCurrency,
+    favourites,
+    handleFavourites,
+    title
+}) => {
   return (
-    <div>Dropdown</div>
+    <div>
+        <label className='block text-sm font-medium text-gray-700'>{title}</label>
+        <div className='mt-1 relative'>
+
+            <hr></hr>
+            <select className="w-full bg-gray-200 p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"    >
+                {currencies?.map((curr)=>{return (
+                <option value={curr} key={curr}>
+                    {curr}
+                </option>
+                )
+            })}
+            </select>
+            <button 
+            onClick={()=>handleFavourites(currency)}
+            className="absolute inset-y-0 right-0 pr-5 flex items-center text-sm leading-5"><HiOutlineStar /></button>
+
+        </div>
+    </div>
   )
 }
 
